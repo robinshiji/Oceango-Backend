378f2d7fc5aa62f31a5ae4d636e5739d29146bd0
robinshiji <robinshiji3@gmail.com>
2026-09-24 12:58:49 +0530

    added admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Hub, Customer, Parcel, ParcelTrackingLog, WhatsAppLog, Setting
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'name', 'phone', 'role', 'hub', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('hub', 'name', 'phone', 'role')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Custom Fields', {'fields': ('hub', 'name', 'phone', 'role')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Hub)
+admin.site.register(Customer)
+admin.site.register(Parcel)
+admin.site.register(ParcelTrackingLog)
+admin.site.register(WhatsAppLog)
+admin.site.register(Setting)
